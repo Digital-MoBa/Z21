2436eebb7c7de99ab6c22fb862dde4d2a39f5d56
Filippo Gentile <gentilefilippo01@gmail.com>
2023-06-25 01:43:05 +0200

    Run clang-format 16

--- z21nvs.cpp
+++ z21nvs.cpp
@@ -1,13 +1,13 @@
 /*
 *****************************************************************************
-  *		z21nvs.cpp - library for emulate EEPROM on Non-Volatile Storage
-  *		Copyright (c) 2021 Philipp Gahtow  All right reserved.
-  *
-  *
+*		z21nvs.cpp - library for emulate EEPROM on Non-Volatile Storage
+*		Copyright (c) 2021 Philipp Gahtow  All right reserved.
+*
+*
 *****************************************************************************
-  * IMPORTANT:
-  * 
-  * 	Please contact ROCO Inc. for more details.
+* IMPORTANT:
+*
+* 	Please contact ROCO Inc. for more details.
 *****************************************************************************
 */
 
@@ -25,62 +25,61 @@ z21nvsClass::z21nvsClass()
   // has to use a namespace name to prevent key name collisions. We will open storage in
   // RW-mode (second parameter has to be false).
   // Note: Namespace name is limited to 15 chars.
-  //store.begin("Z21-app", false);	
+  // store.begin("Z21-app", false);
 }
 
 // Public Methods //////////////////////////////////////////////////////////////
 // Functions available in Wiring sketches, this library, and other libraries
 
 //*********************************************************************************************
-//Daten ermitteln
-uint8_t z21nvsClass::read(uint16_t adr) 
+// Daten ermitteln
+uint8_t z21nvsClass::read(uint16_t adr)
 {
-	store.begin("Z21-app", true);
-	
-	//size_t schLen = store.getBytes("Z21EEPROM", NULL, NULL);
-    char buffer[EESize];
-    store.getBytes("Z21EEPROM", buffer, EESize);
-	
-	// Close the Preferences
-	store.end();
-	
-	if (adr < EESize)
-		return buffer[adr];
-	return 0xFF;
+  store.begin("Z21-app", true);
+
+  // size_t schLen = store.getBytes("Z21EEPROM", NULL, NULL);
+  char buffer[EESize];
+  store.getBytes("Z21EEPROM", buffer, EESize);
+
+  // Close the Preferences
+  store.end();
+
+  if (adr < EESize)
+    return buffer[adr];
+  return 0xFF;
 }
 
-bool z21nvsClass::write(uint16_t adr, uint8_t value) 
+bool z21nvsClass::write(uint16_t adr, uint8_t value)
 {
-	store.begin("Z21-app", false);
-	
-	//size_t schLen = store.getBytes("Z21EEPROM", NULL, NULL);
-    char buffer[EESize];
-    store.getBytes("Z21EEPROM", buffer, EESize);
-	
-	
-	
-	if (adr < EESize) {
-		buffer[adr] = value;
-		store.putBytes("Z21EEPROM", buffer, EESize);
-		// Close the Preferences
-		store.end();
-		
-		return true;
-	}
-	else {
-		// Close the Preferences
-		store.end();
-		return false;
-	}
+  store.begin("Z21-app", false);
+
+  // size_t schLen = store.getBytes("Z21EEPROM", NULL, NULL);
+  char buffer[EESize];
+  store.getBytes("Z21EEPROM", buffer, EESize);
+
+  if (adr < EESize)
+  {
+    buffer[adr] = value;
+    store.putBytes("Z21EEPROM", buffer, EESize);
+    // Close the Preferences
+    store.end();
+
+    return true;
+  }
+  else
+  {
+    // Close the Preferences
+    store.end();
+    return false;
+  }
 }
 
 bool z21nvsClass::begin(uint16_t size)
 {
-	
-	return true;
+  return true;
 }
 
 bool z21nvsClass::commit(void)
 {
-	return true;
+  return true;
 }
--- z21nvs.h
+++ z21nvs.h
@@ -5,35 +5,35 @@
 
 // include types & constants of Wiring core API
 #if defined(WIRING)
- #include <Wiring.h>
+#  include <Wiring.h>
 #elif ARDUINO >= 100
- #include <Arduino.h>
+#  include <Arduino.h>
 #else
- #include <WProgram.h>
+#  include <WProgram.h>
 #endif
 
 #include <Preferences.h>
 
-#define EESize 512    //Größe des EEPROM
+#define EESize 512 // Größe des EEPROM
 
 // library interface description
 class z21nvsClass
 {
   // user-accessible "public" interface
-  public:
-	z21nvsClass(void);	//Constuctor
-	
-	bool begin(uint16_t size);
-	
-	uint8_t read(uint16_t adr);
-	bool write(uint16_t adr, uint8_t value);
-	
-	bool commit(void);
-	
-/*	
-  // library-accessible "private" interface
-  private:
-  
-	//uint8_t storage[EESize];
-*/
+public:
+  z21nvsClass(void); // Constuctor
+
+  bool begin(uint16_t size);
+
+  uint8_t read(uint16_t adr);
+  bool write(uint16_t adr, uint8_t value);
+
+  bool commit(void);
+
+  /*
+    // library-accessible "private" interface
+    private:
+
+      //uint8_t storage[EESize];
+  */
 };
